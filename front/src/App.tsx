6c74e7c9f11022b862645ed5b76dc834eb684c28
Julien-epi <julienranson77@gmail.com>
2023-02-15 14:16:54 +0100

    config tailwinds for front adding services (axios) + form for register - REGISTER DONE

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,17 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import { Toaster } from "react-hot-toast";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mt-3">
+        <Routes>
+          <Route path="/createUser" element={<Register />} />
+        </Routes>
+      </div>
+      <Toaster />
     </div>
   );
 }
---
+++ front/src/types/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+  }
+  
+  export interface RegisterForm {
+    firstname: string;
+    lastname: string;
+    email: string;
+  }
